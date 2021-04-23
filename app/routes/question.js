8d293bc691e41418f16746ab316ab1aab3c8a80a
Sensanaty <salevic@lu-ka.me>
2021-04-23 16:34:59 +0200

    Conditionally return the user ID hash in the question model

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,10 +5,16 @@ import { inject as service } from "@ember/service";
 export default Route.extend({
   currentSession: service(),
   model(params) {
-    return RSVP.hash({
-      question: this.store.findRecord("question", params.question_id, { include: "answers" }),
-      currentUser: this.currentSession.user,
-      userId: parseInt(this.currentSession.user.id)
-    })
+    if (this.currentSession.user) {
+      return RSVP.hash({
+        question: this.store.findRecord("question", params.question_id, { include: "answers" }),
+        currentUser: this.currentSession.user,
+        userId: parseInt(this.currentSession.user.id)
+      })
+    } else {
+      return RSVP.hash({
+        question: this.store.findRecord("question", params.question_id, { include: "answers" })
+      });
+    }
   }
 });
